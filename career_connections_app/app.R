f24f7b81d91ed68b04e37cfd5cc960ecdf3a1d1f
Anna <aheiting25@coa.edu>
2024-03-27 13:39:20 +0000

    Add radioButton to exclude unpaid career connections

--- career_connections_app/app.R
+++ career_connections_app/app.R
@@ -24,22 +24,28 @@ CombinedCareerConnections_shiny$Opportunity <- rep(sheets, sapply(SheetList, nro
 ui = fluidPage(
   
   titlePanel("COA Career Connections"),
-# Sidebar Panel ----
+## Sidebar Panel ----
   sidebarLayout(
     
     sidebarPanel = sidebarPanel(
+  # vector: Unpaid internships/jobs
+      radioButtons("exclude_unpaid", label = h3("Unpaid internships"),
+                   choices = c("Exclude", "Include"),
+                   selected = "Include"
+                ),
+      
   # vector: International Students    
       checkboxGroupInput("Open_to_Intl._Students", label = h3("International Students"), 
                          choices = list("Yes" = 1, "No" = 2),
                          selected = 1),
   # vector: Opportunity
-      checkboxGroupInput("Opportunity", label = h3("Type of Opportunity"), 
+      checkboxGroupInput("OpportunityType", label = h3("Type of Opportunity"), 
                          choices = unique(CombinedCareerConnections_shiny$Opportunity),
                          selected = unique(CombinedCareerConnections_shiny$Opportunity),
                          )
     ),
   
-# Main Panel ----    
+## Main Panel ----    
     mainPanel = mainPanel(
       
       dataTableOutput(outputId = 'data_table')
@@ -52,24 +58,33 @@ server = function(input, output){
   output$data_table = renderDataTable({
     filtered_data <- CombinedCareerConnections_shiny
     
+    # Filter data based on Unpaid radioButton
+    if (input$exclude_unpaid == "Exclude") { 
+      filtered_data <- subset(filtered_data, trimws(Pay) != "Unpaid")} 
+    else if (input$exclude_unpaid == "Include") { }
+    
     # Filter data based on International Students checkbox
     if (input$Open_to_Intl._Students == 1) {
-      filtered_data <- subset(filtered_data, `Open_to_Intl._Students` == "Yes" | is.na(`Open_to_Intl._Students`))
+      filtered_data <- subset(filtered_data, 
+                              `Open_to_Intl._Students` == "Yes" | 
+                                is.na(`Open_to_Intl._Students`))
     } else if (input$Open_to_Intl._Students == 2) {
-      filtered_data <- subset(filtered_data, `Open_to_Intl._Students` == "No" | is.na(`Open_to_Intl._Students`))
+      filtered_data <- subset(filtered_data, 
+                              `Open_to_Intl._Students` == "No" | 
+                                is.na(`Open_to_Intl._Students`))
     }
     
     # Filter data based on Opportunity checkbox
-    filtered_data <- CombinedCareerConnections_shiny[
-      CombinedCareerConnections_shiny$Opportunity %in% input$Opportunity, ]
+    filtered_data <- filtered_data[
+      filtered_data$Opportunity %in% input$OpportunityType, ]
     
+    # Datatable basis
     datatable(filtered_data, options = list(
       searching = FALSE, 
       pageLength = 5,
       lengthMenu = c(5, 15, 20, 25)
     ))
   })
-  
 }
 
 shinyApp(ui, server)
